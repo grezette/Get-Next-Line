431daca863a6f03405abf80843e2faf03d2d4117
Gabriel REZETTE <grezette@e1r9p9.42.fr>
2020-01-24 21:25:14 +0100

    Done in 3 fonctions, 0 leaks, and norminette, Just need to beat the record

--- get_next_line.c
+++ get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: grezette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/11 12:56:39 by grezette          #+#    #+#             */
-/*   Updated: 2019/11/24 11:03:06 by grezette         ###   ########.fr       */
+/*   Updated: 2020/01/24 21:23:15 by grezette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
- int	ft_get_that_line(char **line, char *buff, int fctr, int len)
+static int		ft_get_that_line(char **line, char *buff, int fctr, int len)
 {
-	int 	i;
+	int		i;
 	int		j;
 	char	*tmp;
 
@@ -24,7 +24,6 @@
 	if (!(*line = (char *)malloc(sizeof(**line) * (BUFFER_SIZE * fctr + 1))))
 	{
 		free(tmp);
-		tmp = NULL;
 		return (-1);
 	}
 	while (tmp && tmp[++i])
@@ -32,29 +31,27 @@
 	i = (i < 0) ? 0 : i;
 	while (++j < len)
 		(*line)[i + j] = buff[j];
-	(*line)[i + j] = 0;
+	(*line)[i + (j++)] = 0;
 	i = 0;
-	j++;
 	while (buff[j])
 		buff[i++] = buff[j++];
 	buff[i] = 0;
 	free(tmp);
-	tmp = NULL;
 	return (1);
 }
 
-char	*ft_gnl_join(char *line, char *buff, int fctr)
+static char		*ft_gnl_join(char *line, char *buff, int fctr)
 {
-	int 	i;
-	int 	j;
-	char 	*str;
+	int		i;
+	int		j;
+	char	*str;
 
 	i = -1;
 	j = -1;
 	if (!(str = (char *)malloc(sizeof(*str) * (BUFFER_SIZE * fctr + 1))))
 	{
 		free(line);
-		return (line = NULL);
+		return (NULL);
 	}
 	while (line && line[++i])
 		str[i] = line[i];
@@ -64,11 +61,10 @@ char	*ft_gnl_join(char *line, char *buff, int fctr)
 	str[i + j] = 0;
 	buff[0] = 0;
 	free(line);
-	line = NULL;
 	return (str);
 }
 
-int			get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
 	static char	buff[BUFFER_SIZE + 1];
 	int			len;
